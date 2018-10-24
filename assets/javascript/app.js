a56d5eb87116cbbfc37b5391d10f4743a19e0955
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-24 13:49:14 -0600

    gif start and stop on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 //create array to populate initial buttons
-var topics = ["Powerpuff Girls", "Adventure Time", "Winnie The Pooh", "Fraggle Rock", "Jem and the Holograms", "Animaniacs"]
+var topics = ["Powerpuff Girls", "Adventure Time", "Winnie The Pooh", "Bob's Burgers", "Fraggle Rock", "Jem and the Holograms", "Animaniacs"]
 
-
+var stillURL
+var activeURL 
     
 //function to create button for each item
     //for loop to run through array
@@ -30,19 +31,63 @@ function grabGif() {
     // }
     
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + showNoSpace + "&api_key=9hUJ888Kc8SXY9TRfbZNZ6ge8p8GaVs2&limit=10"
-
+    //pull info from api
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        var gifContainer = $("<img>")
         console.log(response);
+
+        var results = response.data
+        
+        
+
+        for(var i=0; i<results.length; i++) {
+            var gifContainer = $("<div>");
+
+            var imageURL = results[i].images.fixed_height_still.url;   
+            var gifImage = $("<img>")
+            gifImage.attr("src", imageURL);
+            gifImage.attr("data-active", results[i].images.fixed_height.url)
+            gifImage.attr("data-still", imageURL); 
+            gifImage.attr("data-state", "still").addClass("gif");
+
+            var p = $("<p>");
+            p.text("Rating: " + results[i].rating)
+
+            gifContainer.append(p);
+            gifContainer.append(gifImage);
+            $(".gifHolder").prepend(gifContainer);
+        }
+
+        // stillURL = results[i].images.fixed_height_still.url
+        // activeURL = results[i].images.fixed_height.url
     })
 }
+
+$(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    console.log(this)
+    if(state === "still") {
+        $(this).attr("src", $(this).attr("data-active"))
+        $(this).attr("data-state", "animate");
+    }
+      else {$(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+
+      }
+});
+
 
 //on click event for search button
 
 
-//on click event for gif buttons
+//on click event for show buttons
 $(document).on("click", ".show", grabGif);
-//pull info from api
+
+//on click for gifs to activate
+
+
+    //add data-state still attribute
+
+//ask why i had to use results instead of response.data.etc
